02a0c8809c06494c72fcb134e9c1d996128b2240
Ahmed Sereta <seretaahmed18@gmail.com>
2024-04-22 18:09:55 +0300

    Added invoice screen

--- app/services/api.js
+++ app/services/api.js
@@ -226,3 +226,32 @@ export const fetchAllPayments = async () => {
         throw new Error("Failed to fetch Payments data due to network or server error."); // Provide user-friendly error message
     }
 };
+
+// Paymentss Action
+
+export const fetchAllInvoice = async () => {
+    const token = await AsyncStorage.getItem(TOKEN_KEY); // Retrieve the token
+
+    if (!token) {
+        console.error("No access token available."); // Log and handle cases where no token is found
+        throw new Error("Authentication token is not available.");
+    }
+
+    try {
+        // Make an HTTP GET request to fetch all Payments data
+        const response = await Axios.get(`${BASE_URL}/api/invoice/`, {
+            headers: {
+                Authorization: `Bearer ${token}`, // Use the retrieved token for authorization
+            },
+        });
+
+        if (response.status === 200) {
+            return response.data; // Return the fetched data directly if the request is successful
+        } else {
+            throw new Error(`Failed to fetch Invoice data: Status code ${response.status}`); // Provide detailed error info
+        }
+    } catch (error) {
+        console.error("Error fetching Invoice data:", error); // Log the error for debugging
+        throw new Error("Failed to fetch Invoice data due to network or server error."); // Provide user-friendly error message
+    }
+};
