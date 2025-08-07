0f6b0e4a2b9fb20e5bacf9b2829f5e30a9dacc20
Ahmed Sereta <serenade18@gmail.com>
2025-08-07 10:36:34 +0300

    Updated icons

--- constants/images.js
+++ constants/images.js
@@ -1,7 +1,7 @@
 import cards from "../assets/images/cards.png";
 import path from "../assets/images/path.png";
-import logo from "../assets/images/logo.png";
-import logoSmall from "../assets/images/user2.png";
+import logo from "../assets/images/almalogo.png";
+import logoSmall from "../assets/images/almalogo.png";
 import empty from "../assets/images/empty.png";
 import profile from "../assets/images/user1.png";
 
